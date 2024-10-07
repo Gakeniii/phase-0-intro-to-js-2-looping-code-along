7aba4a7f91a6a31020d8fee9348995b4bccb5130
Sifa Gakeni Muriithi <gakenisifa@gmail.com>
2024-10-07 15:15:25 +0300

    loping code

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+//write your code here
+
+const names = ["Ellie", "Jolene", "Merlin"];
+const writeCards = function(name,surprise){
+    const messages = [];
+
+    for(let i=0; i<names.length; i++){
+        const message = `Thank you, ${name[i]}, for the wonderful ${surprise} gift!`;
+        messages.push(message);
+    }
+
+    return messages;
+}
+console.log(writeCards(names, "surprise"));
+
+const countDown = function(){
+    for(let i = 0; i < 11; i++){
+        console.log(i)
+    }
+}
+
